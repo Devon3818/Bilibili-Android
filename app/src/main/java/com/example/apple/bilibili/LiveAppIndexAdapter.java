edd78c4e7ad4e821001d6ece27a9449087076a1a
Devon <k849996781@vip.qq.com>
2018-04-01 00:08:14 +0800

    ADD com.youth.banner.Banner

--- app/src/main/java/com/example/apple/bilibili/LiveAppIndexAdapter.java
+++ app/src/main/java/com/example/apple/bilibili/LiveAppIndexAdapter.java
@@ -5,6 +5,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.Banner;
+import com.youth.banner.loader.ImageLoader;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by apple on 2018/3/31.
@@ -13,6 +21,7 @@ import android.view.ViewGroup;
 public class LiveAppIndexAdapter extends RecyclerView.Adapter {
 
     Context mContext;
+    List imgList = new ArrayList();
 
     public LiveAppIndexAdapter(Context context){
         mContext = context;
@@ -20,7 +29,11 @@ public class LiveAppIndexAdapter extends RecyclerView.Adapter {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ItemViewHolder(LayoutInflater.from(mContext).inflate(R.layout.test_view,parent,false));
+        if(viewType == 1 ){
+            return new LiveBannerViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_live_banner,parent,false));
+        }else{
+            return new ItemViewHolder(LayoutInflater.from(mContext).inflate(R.layout.test_view2,parent,false));
+        }
     }
 
     @Override
@@ -30,7 +43,16 @@ public class LiveAppIndexAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemCount() {
-        return 50;
+        return 10;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if( position == 0 ){
+            return 1;
+        }else{
+            return 0;
+        }
     }
 
     class ItemViewHolder extends RecyclerView.ViewHolder {
@@ -39,4 +61,26 @@ public class LiveAppIndexAdapter extends RecyclerView.Adapter {
             super(itemView);
         }
     }
+
+    class LiveBannerViewHolder extends RecyclerView.ViewHolder {
+
+        public LiveBannerViewHolder(View itemView) {
+            super(itemView);
+            imgList.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic1xjab4j30ci08cjrv.jpg");
+            imgList.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic1xjab4j30ci08cjrv.jpg");
+            Banner banner = (Banner) itemView.findViewById(R.id.banner);
+            banner.setImages(imgList).setImageLoader(new GlideImageLoader()).start();
+        }
+    }
+
+    class GlideImageLoader extends ImageLoader {
+
+        @Override
+        public void displayImage(Context context, Object path, ImageView imageView) {
+            Glide.with(context).load(path).into(imageView);
+        }
+    }
+
+
+
 }
